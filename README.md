c8516d9f5712258aa73fceb0188fa602664b3790
Raghav-KM <mkumar.raghav2002@gmail.com>
2024-06-22 14:10:20 +0530

    Day 1 : Reading File

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # Compiler-Design
-
-
+g++ -o ../build/compiler main.cpp && ../build/compiler ../input/source.dc
 
 # 22-06-2024
 - Read a file that is passed as an argument 
--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,14 @@
 
 using namespace std;
 
+
+enum class TOKENS{
+  tRETURN,
+  tINT_LIT,
+  tSEMICOLON
+};
+
+
 int main(int argc, char *argv[]) {
 
   if (argc != 2) {
@@ -12,11 +20,17 @@ int main(int argc, char *argv[]) {
     return EXIT_FAILURE;
   }
 
+  string input_file_path = argv[1];
+  ifstream f_input(input_file_path);
+  if(f_input.fail()){
+    cerr<<"ERR : Error while reading file"<<endl;
+    return EXIT_FAILURE;
+  }
+  
   cout << "Reading " << argv[1] << endl;
-
-  ifstream input(argv[1]);
   stringstream input_buffer;
-  input_buffer << input.rdbuf();
+  input_buffer << f_input.rdbuf();
+  f_input.close();
 
   cout << input_buffer.str() << endl;
   return EXIT_SUCCESS;
