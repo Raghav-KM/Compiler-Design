9a6462562c7184d7865ac2e4588be336869ecf58
Raghav <Raghav-KM>
2024-08-04 12:19:28 +0530

    04-08-2024 : Working scope (Tested)

--- build/Assembly/program.asm
+++ build/Assembly/program.asm
@@ -5,11 +5,9 @@ section .text
 
 _start:
     mov eax, 0
-    mov [i_0], eax
-    mov eax, 0
-    mov [j_1], eax
+    mov [i_3], eax
 _for1:
-    mov eax, [j_1]
+    mov eax, [i_3]
     mov ebx, 5
     cmp eax, ebx
     mov eax, 0
@@ -21,18 +19,31 @@ _cmp1_end:
     cmp eax, 0
     jz _for1_end
 
-    mov eax, [j_1]
-    add eax, 1
+    call funcA
+    mov eax, [i_3]
+    mov ebx, 4
+    cmp eax, ebx
+    mov eax, 0
+    jge _cmp2_end
+    mov eax, 1
+_cmp2_end:
     mov [_t2], eax
     mov eax, [_t2]
-    mov [i_1], eax
-    mov eax, [i_1]
-    call _print_integer_subroutine
-    mov eax, [j_1]
+    cmp eax, 1
+    jne _if1
+
+    mov al, 10
+    call _print_character_subroutine
+
+    jmp _if1_end
+
+_if1:
+_if1_end:
+    mov eax, [i_3]
     add eax, 1
     mov [_t1], eax
     mov eax, [_t1]
-    mov [j_1], eax
+    mov [i_3], eax
     jmp _for1
 
 _for1_end:
@@ -43,40 +54,47 @@ _for1_end:
     xor ebx, ebx
     int 0x80
   
-_print_integer_subroutine:
-    push eax
-    mov ecx, buffer + 10
-
-    xor edx, edx
-    mov ebx, 10
-
+funcA:
+    mov eax, 97
+    mov [i_2], eax
+_for2:
+    mov eax, [i_2]
+    mov ebx, 122
+    cmp eax, ebx
+    mov eax, 0
+    jg _cmp3_end
+    mov eax, 1
+_cmp3_end:
+    mov [_t1], eax
+    mov eax, [_t1]
     cmp eax, 0
-    jge ._convert_loop
-    neg eax
+    jz _for2_end
 
-._convert_loop:
-    xor edx, edx
-    div ebx
-    add dl, '0'
-    mov [ecx], dl
-    dec ecx
-    test eax, eax
-    jnz ._convert_loop
+    mov al, [i_2]
+    call _print_character_subroutine
 
-    pop eax
-    cmp eax, 0
-    jge ._print
-    mov byte [ecx], '-'
-    dec ecx
+    mov al, 32
+    call _print_character_subroutine
 
-._print:
-    inc ecx
-    mov edx, buffer + 12
-    sub edx, ecx
-    mov eax, 4
-    mov ebx, 1
-    int 0x80
+    mov eax, [i_2]
+    add eax, 1
+    mov [_t1], eax
+    mov eax, [_t1]
+    mov [i_2], eax
+    jmp _for2
+
+_for2_end:
+
     ret
+
+_print_character_subroutine:
+    mov ecx, char_buffer 
+    mov [ecx], al
+    mov edx, 1 
+    mov eax, 4 
+    mov ebx, 1 
+    int 0x80 
+    ret 
 
 
 _print_newline_subroutine:
@@ -89,11 +107,10 @@ _print_newline_subroutine:
     ret
 
 section .bss
-    buffer  resb 12
+    char_buffer resb 1
     newline resb 1
-    i_0 resd 1
-    j_1 resd 1
-    i_1 resd 1
+    i_3 resd 1
+    i_2 resd 1
     _t1 resd 1
     _t2 resd 1
 
